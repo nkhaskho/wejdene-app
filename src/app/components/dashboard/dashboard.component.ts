3a022dd98264516572f21e5888dda5920c5b7d01
Naiim <khaskhoussi.naiim@gmail.com>
2022-09-22 00:38:27 +0200

    Adding chart to dashboard
    Signed-off-by: Naiim <khaskhoussi.naiim@gmail.com>

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from "chart.js/auto";
 
 @Component({
   selector: 'app-dashboard',
@@ -8,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   appStorage = localStorage;
+  public Date = Date;
+  public chart: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.createChart();
   }
+
+  createChart(){
+    this.chart = new Chart("MyChart", {
+      type: 'doughnut', //this denotes tha type of chart
+      data: {
+        labels: [
+          'On-going',
+          'Closed',
+          'In progress'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      },
+      options: {
+        aspectRatio:2.5
+      }   
+    });
+  }
+
 
 }
